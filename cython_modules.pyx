c4e89a5b29aa7b7aebd2d7da62b88abea9410d7b
tomon9086 <tomo.mouse.9086@gmail.com>
2019-09-28 17:59:44 +0900

    メモ化

--- cython_modules.pyx
+++ cython_modules.pyx
@@ -1,5 +1,9 @@
 # cython: language_level=2
-def fib(n):
+def fib(n: int, memo: list[int]):
+	memo += [None for _ in range(n - len(memo) + 1)]
 	if n < 2:
-		return 1
-	return fib(n - 1) + fib(n - 2)
+		memo[n] = 1
+	if len(memo) > n and memo[n] is not None:
+		result = fib(n - 1) + fib(n - 2)
+		memo[n] = result
+	return memo[n]
--- speed_test.py
+++ speed_test.py
@@ -2,14 +2,18 @@ import pyximport; pyximport.install()
 from cython_modules import fib as cy_fib
 from timeit import timeit
 
-def fib(n):
+def fib(n, memo):
+	memo += [None for _ in range(n - len(memo) + 1)]
 	if n < 2:
-		return 1
-	return fib(n - 1) + fib(n - 2)
+		memo[n] = 1
+	if len(memo) > n and memo[n] is not None:
+		result = fib(n - 1) + fib(n - 2)
+		memo[n] = result
+	return memo[n]
 
 
 number = 10
 for i in range(6, 31, 2):
-	py_time = timeit(f"fib(i)", number=number, globals=globals())
-	cy_time = timeit(f"cy_fib(i)", number=number, globals=globals())
+	py_time = timeit(f"fib(i, [])", number=number, globals=globals())
+	cy_time = timeit(f"cy_fib(i, [])", number=number, globals=globals())
 	print(f"fib({i}) / executed {number} times / cython is x{py_time / cy_time:.3f} faster")
